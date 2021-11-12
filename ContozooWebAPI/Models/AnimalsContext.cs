514f005f28229840d293afd961e57a0c3ef2ac43
caitlin-ohara <caitlinohara@microsoft.com>
2021-11-12 13:36:17 +0000

    inital commit

---
+++ ContozooWebAPI/Models/AnimalsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContozooWebAPI.Models;
+
+
+    public class AnimalsContext : DbContext
+    {
+        public AnimalsContext(DbContextOptions<AnimalsContext> options) : base(options)
+        {}
+        public DbSet<ContozooWebAPI.Models.Animals> Animals{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+        }
+    }
+
