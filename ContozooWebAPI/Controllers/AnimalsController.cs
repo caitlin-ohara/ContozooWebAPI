2a999bbeb231988da46c6d83a4b66b1890019ec3
caitlin-ohara <caitlinohara@microsoft.com>
2021-11-12 12:25:16 +0000

    Add project files.

---
+++ ContozooWebAPI/Controllers/AnimalsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ContozooWebAPI.Models;
+
+namespace ContozooWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly AnimalsContext _context;
+
+        public AnimalsController(AnimalsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Animals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Animals>>> GetZooAnimals()
+        {
+            return await _context.Animals.ToListAsync();
+        }
+
+        // GET: api/Animals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animals>> GetAnimals(long id)
+        {
+            var animals = await _context.Animals.FindAsync(id);
+
+            if (animals == null)
+            {
+                return NotFound();
+            }
+
+            return animals;
+        }
+
+        // PUT: api/Animals/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAnimals(long id, Animals animals)
+        {
+            if (id != animals.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(animals).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Animals
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Animals>> PostAnimals(Animals animals)
+        {
+            _context.Animals.Add(animals);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAnimals), new { id = animals.Id }, animals);
+        }
+
+        // DELETE: api/Animals/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Animals>> DeleteAnimals(long id)
+        {
+            var animals = await _context.Animals.FindAsync(id);
+            if (animals == null)
+            {
+                return NotFound();
+            }
+
+            _context.Animals.Remove(animals);
+            await _context.SaveChangesAsync();
+
+            return animals;
+        }
+
+        private bool AnimalsExists(long id)
+        {
+            return _context.Animals.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ContozooWebAPI/Models/Animals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ContozooWebAPI.Models
+{
+    public class Animals
+    {
+        public int Id { get; set; }
+        public string AnimalName { get; set; }
+        public string Number { get; set; }
+        public string Location { get; set; }
+        public string ActiveHours { get; set; }
+        public string Notes { get; set; }
+        
+    }
+}
